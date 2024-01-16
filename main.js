4d67179f49530d372dcd0000a0bc68ca57a9ce4d
PaolaSL12 <paola.sanchezl12@gmail.com>
2024-01-16 19:59:31 +0100

    set navbar

---
+++ main.js
@@ -0,0 +1,7 @@
+// https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
+// whdPS3v7rZ98nsGSfSnkXCGqsLd__vhzkUJXSKt_2bo
+// <https://api.unsplash.com/search/photos?page=1&query=office>
+
+import { NavBar } from "./src/Components/NavBar/NanBar";
+
+NavBar()

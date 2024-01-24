5720f5bc9d27e2f5a952cbdc3c27677237440246
PaolaSL12 <paola.sanchezl12@gmail.com>
2024-01-24 10:17:20 +0100

    fix input alert

--- src/Components/SearchImg/SearchImg.js
+++ src/Components/SearchImg/SearchImg.js
@@ -10,9 +10,11 @@ export const searchImg = (input) => {
         const url = "https://api.unsplash.com/search/photos?page=1&query=" + randomWords[(Math.floor(Math.random() * 4))] + "&per_page=12&client_id=whdPS3v7rZ98nsGSfSnkXCGqsLd__vhzkUJXSKt_2bo" 
         
         fetch(url)
-        .then((res) => {
+        .then((res) => {;
             if (res.ok) {
+               
                 return res.json()
+                
             } else {
                 alert(res.status)
             }
@@ -26,13 +28,20 @@ export const searchImg = (input) => {
         fetch(url)
         .then((res) => {
             if (res.ok) {
+                
                 return res.json()
             } else {
+
                 alert(res.status)
             }
         })
         .then((res) => {
+           if (res.results.length === 0) {
+             alert("introduzca una palabra valida en el buscador")
+           } else {
             containerfill(res.results, selectContainer())
+           }
+            
             })
     }
 }
